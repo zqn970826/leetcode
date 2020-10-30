700bb583415eeda51cfe20377d9bd1edeb596bd6
skye <qiunan.zou@skye.work>
2020-10-30 16:20:52 +0800

    stack、queue、vector

---
+++ queue.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <queue>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+	queue<int> Q;
+	Q.push(1);
+	Q.push(2);
+	Q.push(3);
+	Q.push(4);
+
+	printf("%ld\n", Q.size());
+	printf("%d\n", Q.front());
+	Q.pop();
+	printf("%d\n", Q.front());
+	Q.pop();
+	printf("%d\n", Q.front());
+	Q.pop();
+	printf("%d\n", Q.front());
+	Q.pop();
+	printf("%d\n", Q.empty());
+	return 0;
+}
